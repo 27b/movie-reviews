6391c10fd8db8011c4255444bb6762ddfc65d34a
27b <orionbaldassani27@gmail.com>
2024-07-11 12:17:11 -0300

    Added database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,18 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { MoviesModule } from './movies/movies.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: "sqlite",
-      database: `./database.sqlite`,
-      entities: [__dirname + '/../**/*.entity.js'],
-      logging: true,
-      synchronize: true
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
     }),
+    DatabaseModule,
     MoviesModule,
     UsersModule,
     AuthModule
